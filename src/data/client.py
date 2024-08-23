2cdfbbf39129140d49e1d4feb6b3c725cc4f69f2
Charlies-Kvit <theivangao@gmail.com>
2024-08-23 14:08:47 +0300

    изменил чуть бд и requirements.txt

--- src/data/client.py
+++ src/data/client.py
@@ -4,7 +4,7 @@ from .db_session import SqlAlchemyBase
 from werkzeug.security import generate_password_hash, check_password_hash
 
 
-class User(SqlAlchemyBase):
+class Client(SqlAlchemyBase):
     __tablename__ = 'users'
 
     id = sqlalchemy.Column(sqlalchemy.Integer,
